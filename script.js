0475e43ddcfc1523325da91c5257547b3b6b0c0a
chfd1 <68733259+chfd1@users.noreply.github.com>
2020-08-09 05:38:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Script for the language page
+
+// Makes the game in English
+function playing_in_english() {
+    document.getElementById('h1_language_page').innerHTML = 'Welcome!';
+    document.getElementById('h3_language_page').innerHTML = 'Want to play sudoku ?';
+    document.getElementById('p_language_page').innerHTML = 'Choose your language';
+    document.getElementById('Lets_go_button').innerHTML = 'Lets go';
+}
+
+// Makes the game in Hebrew
+function playing_in_hebrew() {
+    document.getElementById('h1_language_page').innerHTML = '! ברוך הבא';
+    document.getElementById('h3_language_page').innerHTML = '? רוצה לשחק סודוקו';
+    document.getElementById('p_language_page').innerHTML = 'תבחר את השפה שלך';
+    document.getElementById('Lets_go_button').innerHTML = 'בא נתחיל';
+}
+
+// Moves to the login page
+function landing_page() {
+    document.getElementById('language_page').style.display = 'none';
+    document.getElementById('landing_page').style.display = 'grid';
+}
